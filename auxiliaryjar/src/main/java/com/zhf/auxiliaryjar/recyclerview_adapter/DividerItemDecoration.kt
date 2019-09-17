8b4ad4311b97a4c26503cf70cb915e439637b657
zhf <578121695@qq.com>
2019-09-17 17:44:15 +0800

    添加RecyclerView的分割线

---
+++ auxiliaryjar/src/main/java/com/zhf/auxiliaryjar/recyclerview_adapter/DividerItemDecoration.kt
@@ -0,0 +1,97 @@
+package com.zhf.auxiliaryjar.recyclerview_adapter
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+
+
+/**
+* RecyclerView 添加 LinearLayout的分割线
+* Created by zhf on 2019/8/19 14:27
+ *
+ * use:
+ *   val itemDecoration = DividerItemDecoration( context!!, LinearLayoutManager.VERTICAL )
+ *   mRecyclerView!!.addItemDecoration(itemDecoration)
+*/
+
+class DividerItemDecoration(context: Context, orientation: Int) : RecyclerView.ItemDecoration() {
+
+    private val mDivider: Drawable?
+
+    private var mOrientation: Int = 0
+
+    init {
+        val a = context.obtainStyledAttributes(ATTRS)
+        mDivider = a.getDrawable(0)
+        a.recycle()
+        setOrientation(orientation)
+    }
+
+    fun setOrientation(orientation: Int) {
+        if (orientation != HORIZONTAL_LIST && orientation != VERTICAL_LIST) {
+            throw IllegalArgumentException("invalid orientation")
+        }
+        mOrientation = orientation
+    }
+
+    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        if (mOrientation == VERTICAL_LIST) {
+            drawVertical(c, parent)
+        } else {
+            drawHorizontal(c, parent)
+        }
+    }
+
+    fun drawVertical(c: Canvas, parent: RecyclerView) {
+        val left = parent.paddingLeft
+        val right = parent.width - parent.paddingRight
+
+        val childCount = parent.childCount
+        for (i in 0 until childCount) {
+            val child = parent.getChildAt(i)
+            val params = child
+                .layoutParams as RecyclerView.LayoutParams
+            val top = child.bottom + params.bottomMargin
+            val bottom = top + mDivider!!.intrinsicHeight
+            mDivider.setBounds(left, top, right, bottom)
+            mDivider.draw(c)
+        }
+    }
+
+    fun drawHorizontal(c: Canvas, parent: RecyclerView) {
+        val top = parent.paddingTop
+        val bottom = parent.height - parent.paddingBottom
+
+        val childCount = parent.childCount
+        for (i in 0 until childCount) {
+            val child = parent.getChildAt(i)
+            val params = child
+                .layoutParams as RecyclerView.LayoutParams
+            val left = child.right + params.rightMargin
+            //            绘制
+            val right = left + mDivider!!.intrinsicHeight
+            mDivider.setBounds(left, top, right, bottom)
+            mDivider.draw(c)
+        }
+    }
+
+    override fun getItemOffsets(outRect: Rect, itemPosition: Int, parent: RecyclerView) {
+        if (mOrientation == VERTICAL_LIST) {
+            outRect.set(0, 0, 0, mDivider!!.intrinsicHeight)
+        } else {
+            outRect.set(0, 0, mDivider!!.intrinsicWidth, 0)
+        }
+    }
+
+    companion object {
+
+        private val ATTRS = intArrayOf(android.R.attr.listDivider)
+
+        val HORIZONTAL_LIST = LinearLayoutManager.HORIZONTAL
+
+        val VERTICAL_LIST = LinearLayoutManager.VERTICAL
+    }
+}
